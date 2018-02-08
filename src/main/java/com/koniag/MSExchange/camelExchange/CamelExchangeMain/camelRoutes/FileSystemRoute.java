f6a2c071b1a62d6e5d052ed49b33451c17d150cc
Armando Perea <Armando.Perea@noaa.gov>
2018-02-08 16:35:02 -0500

    ggg

--- src/main/java/com/koniag/MSExchange/camelExchange/CamelExchangeMain/camelRoutes/FileSystemRoute.java
+++ src/main/java/com/koniag/MSExchange/camelExchange/CamelExchangeMain/camelRoutes/FileSystemRoute.java
@@ -1,6 +1,9 @@
 package com.koniag.MSExchange.camelExchange.CamelExchangeMain.camelRoutes;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.file.GenericFile;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,9 +11,21 @@ public class FileSystemRoute extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-//		from("smb://alper@FRED/myshare2?password=ceuVceth!1").to("file://target/test-reports");
-		from("smb://AMER;devsu:C$C123$dev@cscgsxaus1v15/public/amertest").to("file://target/recieved-files");
-
+//		from("smb://AMER;devsu:C$C123$dev@cscgsxaus1v15/public/amertest").to("file://target/recieved-files");
+		from("smb://alper@FRED/myshare2?password=ceuVceth!1").process(new Processor() {
+			  public void process(Exchange exchange) throws Exception {
+				    GenericFile body = (GenericFile) exchange.getIn().getBody();
+				    System.out.println("derp "+body.getFileName());
+				  }
+				}).end();
 	}
+/*
+ * 
+		from("file:{{folder.status-files.root}}{{file.consumer.common.properties}}").log("agent-npn start")
+				.unmarshal(npnDataformat).removeProperty("CamelFileExchangeFile").split().simple("${body}")
+				.setHeader("agentNumber").simple("${body.agentNumber}").setHeader("nationalProducerNumber")
+				.simple("${body.nationalProducerNumber}").to("bean:npnRead?method=loadnpndata").end()
+				.log("agent-npn extract done").setId("beanIOListSplitter");
 
+ */
 }
