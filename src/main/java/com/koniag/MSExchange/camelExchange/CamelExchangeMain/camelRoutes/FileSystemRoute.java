7bf858b60647df92ccea2a1d2ee32a5cfae04942
Armando Perea <Armando.Perea@noaa.gov>
2018-02-08 15:40:09 -0500

    hi

--- src/main/java/com/koniag/MSExchange/camelExchange/CamelExchangeMain/camelRoutes/FileSystemRoute.java
+++ src/main/java/com/koniag/MSExchange/camelExchange/CamelExchangeMain/camelRoutes/FileSystemRoute.java
@@ -8,7 +8,9 @@ public class FileSystemRoute extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-		from("smb://xxx;xxx/myshare?password=xxx").to("file://target/test-reports");
+//		from("smb://alper@FRED/myshare2?password=ceuVceth!1").to("file://target/test-reports");
+		from("smb://AMER;devsu:C$C123$dev@cscgsxaus1v15/public/amertest").to("file://target/test-reports");
+
 	}
 
 }
